0daadeaf778bdece230504e9b555ef72f5f2fac5
Matt Magoffin <git+matt@msqr.us>
2015-07-17 12:25:50 +1200

    Initial project skeleton.

---
+++ Menu/Pods/Target Support Files/Pods-Menu-Masonry/Pods-Menu-Masonry-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Menu_Masonry : NSObject
+@end
+@implementation PodsDummy_Pods_Menu_Masonry
+@end
---
+++ Menu/Pods/Target Support Files/Pods-Menu/Pods-Menu-environment.h
@@ -0,0 +1,44 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// BRCocoaLumberjack
+#define COCOAPODS_POD_AVAILABLE_BRCocoaLumberjack
+#define COCOAPODS_VERSION_MAJOR_BRCocoaLumberjack 1
+#define COCOAPODS_VERSION_MINOR_BRCocoaLumberjack 9
+#define COCOAPODS_VERSION_PATCH_BRCocoaLumberjack 0
+
+// BREnvironment
+#define COCOAPODS_POD_AVAILABLE_BREnvironment
+#define COCOAPODS_VERSION_MAJOR_BREnvironment 1
+#define COCOAPODS_VERSION_MINOR_BREnvironment 1
+#define COCOAPODS_VERSION_PATCH_BREnvironment 0
+
+// CocoaLumberjack
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack 9
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack 2
+
+// CocoaLumberjack/Core
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Core
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack_Core 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Core 9
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Core 2
+
+// CocoaLumberjack/Extensions
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Extensions
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack_Extensions 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Extensions 9
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Extensions 2
+
+// Masonry
+#define COCOAPODS_POD_AVAILABLE_Masonry
+#define COCOAPODS_VERSION_MAJOR_Masonry 0
+#define COCOAPODS_VERSION_MINOR_Masonry 6
+#define COCOAPODS_VERSION_PATCH_Masonry 2
+
