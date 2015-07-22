41b1fefc4ecf7317d4e8ca4529edde1d5fad6ac0
Matt Magoffin <git+matt@msqr.us>
2015-07-22 15:58:07 +1200

    Include latest class additions in umbrella include.

--- Menu/Code/UI.h
+++ Menu/Code/UI.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Blue Rocket. All rights reserved.
 //
 
+#import <BRMenu/UI/BRMenuBackBarButtonItemView.h>
+#import <BRMenu/UI/BRMenuBarButtonItemView.h>
 #import <BRMenu/UI/BRMenuPlusMinusButton.h>
 #import <BRMenu/UI/BRMenuStepper.h>
 #import <BRMenu/UI/BRMenuUIStyle.h>
