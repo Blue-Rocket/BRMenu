1692635d6231af5140ab8f7fec33457886ec41a4
Matt Magoffin <git+matt@msqr.us>
2015-07-23 17:21:40 +1200

    Fix copyright.

--- Menu/Code/UI/BRMenuLeftRightPlacementButton.h
+++ Menu/Code/UI/BRMenuLeftRightPlacementButton.h
@@ -3,7 +3,7 @@
 //  AndFramework
 //
 //  Created by Matt on 4/10/13.
-//  Copyright (c) 2013 Blue Rocket, Inc. All rights reserved.
+//  Copyright (c) 2013 Blue Rocket, Inc. Distributable under the terms of the Apache License, Version 2.0.
 //
 
 #import <UIKit/UIKit.h>
