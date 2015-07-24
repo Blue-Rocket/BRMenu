88c46dddd8bdd7f00504ec0151420b75f3ba0e9e
Matt Magoffin <git+matt@msqr.us>
2015-07-24 14:24:23 +1200

    Start work on root menu ordering view controller.

---
+++ Menu/Code/UI/BRMenuOrderingViewController.h
@@ -0,0 +1,17 @@
+//
+//  BRMenuOrderingViewController.h
+//  Menu
+//
+//  Created by Matt on 24/07/15.
+//  Copyright (c) 2015 Blue Rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BRMenu;
+
+@interface BRMenuOrderingViewController : UITableViewController
+
+@property (nonatomic, strong) BRMenu *menu;
+
+@end
