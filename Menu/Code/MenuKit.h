4d8767f4d9707c0c8fc8a7de732c006bc648ea2e
Matt Magoffin <git+matt@msqr.us>
2015-09-27 09:50:22 +1300

    Make 'All' the default subspec, and add MenuKit.h umbrella header.

---
+++ Menu/Code/MenuKit.h
@@ -0,0 +1,12 @@
+//
+//  MenuKit.h
+//  MenuKit
+//
+//  Created by Matt on 21/07/15.
+//  Copyright (c) 2015 Blue Rocket. Distributable under the terms of the Apache License, Version 2.0.
+//
+
+#import <MenuKit/Core.h>
+#import <MenuKit/AFNetworking.h>
+#import <MenuKit/RestKit.h>
+#import <MenuKit/UI.h>
